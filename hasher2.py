4ef56ef91b74e10cba5d06503eb16c2f7a867629
Josh <Enigmajdp@gmail.com>
2018-03-21 09:40:05 -0400

    Update hasher2.py

--- hasher2.py
+++ hasher2.py
@@ -1,13 +1,13 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import hashlib
 def ntlm(pw):
     print(hashlib.new('md4', pw.encode('utf-16le')).hexdigest())
     
-pfile=raw_input('Enter Filename: ')
+pfile=input('Enter Filename: ')
 ofile=open(pfile)
 for line in ofile:
     if len(line)>0:
         line=line.rstrip('\n')
-        print line
+        print(line)
         ntlm(line)
