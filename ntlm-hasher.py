c61dd0070c7e07fbf858481650add50d19a5f4ee
Josh <Enigmajdp@gmail.com>
2018-03-21 08:24:10 -0400

    Create ntlm-hasher.py file
    Create the unfinished tool

---
+++ ntlm-hasher.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import hashlib,binascii
+def ntlm(pw):
+    hash = hashlib.new('md4', pw.encode('utf-16le')).digest()
+    print binascii.hexlify(hash)
+
+pfile=raw_input('Enter filename ')
+ofile=open(pfile)
+for line in ofile:
+    if len(line)>0:
+        line=line.rstrip('\n')
+        print line
+        ntlm(line)
