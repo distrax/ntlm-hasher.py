85ea89382205952c281e7d08ef5937737d22c3eb
distrax <distraxdevel@gmail.com>
2018-03-21 19:04:16 -0400

    Add new early stage tool for more comprehensive options

---
+++ hash3r.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import hashlib
+#Comprehensive python tool to hash wordlists and phrases
+#Function Section:
+def ntlm(pw):
+    print(hashlib.new('md4', pw.encode('utf-16le')).hexdigest())
+def md5(pw):
+    print(hashlib.md5(pw.encode('utf-8')).hexdigest())
+algo={1:ntlm,
+      2:md5,
+      }
+dos=[('1', 'ntlm'), ('2', 'md5')]
+#for k, v in algo.items():
+#    print(v)
+for a, b in dos:
+    print(a, '=', b)
+try:
+    hash3r=input('Enter number corresponding to desired algorithm: ')
+    hash3r=int(hash3r)
+except:
+    print('Not a valid hash choice.')
+pfile=input('Enter filename: ')
+try:
+    pw=open(pfile)
+except:
+    print('File not found.')
+for line in pw:
+    if len(line)>0:
+        line=line.rstrip('\n')
+        print(line)
+        algo[hash3r](line)
