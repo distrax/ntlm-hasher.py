5aaaafb5f869abadb00ed516b5ebd66d1e6ea92f
Josh <Enigmajdp@gmail.com>
2018-03-21 09:42:04 -0400

    Update ntlm-hasher.py

--- ntlm-hasher.py
+++ ntlm-hasher.py
@@ -1,14 +1,14 @@
-#!/usr/bin/python2
+#!/usr/bin/python2.7
 
 import hashlib,binascii
 def ntlm(pw):
     hash = hashlib.new('md4', pw.encode('utf-16le')).digest()
     print binascii.hexlify(hash)
 
-pfile=raw_input('Enter filename ')
+pfile=raw_input('Enter filename: ')
 ofile=open(pfile)
 for line in ofile:
     if len(line)>0:
         line=line.rstrip('\n')
-        print line
+#        print(line)
         ntlm(line)
