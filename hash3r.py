9a3a90acb4fc641fa62f4182dee5b6eae8d197a5
distrax <distraxdevel@gmail.com>
2018-03-22 18:35:46 -0400

    More options

--- hash3r.py
+++ hash3r.py
@@ -6,10 +6,16 @@ def ntlm(pw):
     print(hashlib.new('md4', pw.encode('utf-16le')).hexdigest())
 def md5(pw):
     print(hashlib.md5(pw.encode('utf-8')).hexdigest())
+def sha256(pw):
+    print(hashlib.sha256(pw.encode()).hexdigest())
+def sha512(pw):
+    print(hashlib.sha512(pw.encode()).hexdigest())
 algo={1:ntlm,
       2:md5,
+      3:sha256,
+      4:sha512,
       }
-dos=[('1', 'ntlm'), ('2', 'md5')]
+dos=[('1', 'ntlm'), ('2', 'md5'), ('3', 'sha256'), ('4', 'sha512')]
 #for k, v in algo.items():
 #    print(v)
 for a, b in dos:
